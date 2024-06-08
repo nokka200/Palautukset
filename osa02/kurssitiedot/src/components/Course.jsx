4f6520e03b9963e17db91042df563cd7ffe1ed25
Niku <nokka200@gmail.com>
2024-06-08 13:40:21 +0300

    Muokkaus

--- osa02/kurssitiedot/src/components/Course.jsx
+++ osa02/kurssitiedot/src/components/Course.jsx
@@ -1,18 +1,25 @@
 const Course = ({ course }) => {
+
+    const counter = (acc, cur) => {
+        console.log('what is happening', acc, cur.exercises)
+        return acc + cur.exercises
+    };
+
+    const listItems = course.parts.map(part => 
+        <li key={part.id}>
+            {part.name} {part.exercises}
+        </li>
+    );
+
+    const total = course.parts.reduce((acc, cur) => counter(acc, cur), 0);
+
     return (
         <>
             <h1>{course.name}</h1>
             <ul>
-                {course.parts.map(part =>
-                    <li key={part.id}>
-                        {part.name} {part.exercises}
-                    </li>
-                )}
+                {listItems}
             </ul>
-            <h2>total of {course.parts.reduce((acc, cur) => {
-                //console.log('what is happening', acc, cur.exercises)
-                return acc + cur.exercises;
-            }, 0)} exercises</h2>
+            <h2>total of {total} exercises</h2>
         </>
     )
 };
