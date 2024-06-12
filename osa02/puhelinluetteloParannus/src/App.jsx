134ed35dbb196de92e5625c8431d472aac11a285
Niku <nokka200@gmail.com>
2024-06-12 09:46:38 +0300

    2.11. valmis

--- osa02/puhelinluetteloParannus/src/App.jsx
+++ osa02/puhelinluetteloParannus/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+      })
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
