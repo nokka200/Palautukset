8712a28bba2fcb6cee41dbc56db6b613b941586e
Niku <nokka200@gmail.com>
2024-06-13 15:19:48 +0300

    Yritetään vielä 2.15* tehtävää

--- osa02/puhelinluetteloParannus/src/services/numbers.js
+++ osa02/puhelinluetteloParannus/src/services/numbers.js
@@ -10,7 +10,12 @@ const create = (newObject) => {
 const remove = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`);
     return request.then(response => response.data);
-}
+};
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
+    return request.then(response => response.data);
+};
 
 
-export default { create, remove };
+export default { create, remove, update };
