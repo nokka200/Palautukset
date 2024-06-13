86a7f1d7584f04c805a792dd9474b30ddf773345
Niku <nokka200@gmail.com>
2024-06-13 09:36:57 +0300

    02 d osion loppu

--- Tehtävät/02/01appParannus/src/App.jsx
+++ Tehtävät/02/01appParannus/src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
   useEffect(() => {
     noteService
       .getAll()
-      .then(response => {
-        setNotes(response.data);
+      .then(initialNotes => {
+        setNotes(initialNotes)
       });
   }, []);
 
@@ -25,9 +25,8 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
-        setNewNote('')
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
       });
   };
 
@@ -37,8 +36,14 @@ const App = () => {
 
     noteService
       .update(id, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
       });
   };
 
