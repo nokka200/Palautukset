d09bdf4a64c2323eff29b4a7fe8e7405951aae06
Niku <nokka200@gmail.com>
2024-06-13 14:22:47 +0300

    Aloitettu 2.16

--- osa02/puhelinluetteloParannus/src/App.jsx
+++ osa02/puhelinluetteloParannus/src/App.jsx
@@ -4,12 +4,14 @@ import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import numberService from './services/numbers';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+  const [errorMessage, setErrorMessage] = useState('Iso Virhe')
 
   useEffect(() => {
     console.log('effect');
@@ -72,6 +74,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
 
       <Filter handleSearch={handleSearch} search={search} />
       
